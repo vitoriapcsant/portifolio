b7dd0a8d38e2bc6b9c6341a643dee28824c765fa
vitoriapcsant <vitoriapcsant@gmail.com>
2026-05-05 02:15:37 +0000

    trabalho vitoria e beatriz

--- portfolio/scripts/scripy.js
+++ portfolio/scripts/scripy.js
@@ -1,45 +1,55 @@
 const botao = document.getElementById("modoClaroEscuro");
 console.log(botao);
 
+let claro = true;
 botao.addEventListener("click", function () {
-        let claro = true;
         if (claro) {
-                document.body.style.backgroundColor = "white";
-                document.body.style.color = "black";
-                botao.textContent = "Modo Claro";
-                claro = false;
-        } else {
                 document.body.style.backgroundColor = "black";
                 document.body.style.color = "white";
+                botao.textContent = "Modo Claro";
+
+        } else {
+                document.body.style.backgroundColor = "white";
+                document.body.style.color = "black";
                 botao.textContent = "Modo Escuro";
-                claro = false;
+
         }
         claro = !claro;
 }
 );
 
-const NOME = "Vitoria Pereira";
-let tituloprofissonal = "Garoto de Progama / Faço de tudo";
-let minhaBio = "Sou organizada";
-let anoFormatura = 2028;
+const NOME = "Vitoria e Beatriz";
+let tituloProfissional = "Designer de Jogos Profissional";
+let minhaBio = "Sou um designer de jogos profissional 
+let mesFormatura = 12;
+let diaFormatura = 31;
 let anoIngresso = 2025;
+let mesIngresso = 1;
+let diaIngresso = 1;
+
+let hoje = new Date(); //Dia atual
+let mesAtual = hoje.getMonth() + 1; //Mês atual (getMonth() retorna de 0 a 11, por isso somamos 1)
+let anoAtual = hoje.getFullYear(); //Ano atual
+let diaAtual = hoje.getDate(); //Dia atual
+
 let indefinido;
 let nulo = null;
 let curso = {
-        nome: "Sistema de informação";
+        nome: "Sistemas de Informação",
         semestre: 3,
-        disciplinaAtual: "Desing focado no usuário"
+        disciplina: "Design focado no usuário"
 }
 
 console.log(typeof nulo);
 console.log(typeof indefinido);
 console.log(typeof anoFormatura);
 console.log(typeof minhaBio);
-console.log(typeof tituloProfissonal);
+console.log(typeof tituloProfissional);
 console.log(typeof NOME);
 console.log(typeof curso);
 
 document.getElementById("meuNome").innerText = NOME;
-document.getElementById("tituloProfissonal").innerText = tituloProfissonal;
+document.getElementById("tituloProfissional").innerText = tituloProfissional;
 document.getElementById("minhaBio").innerText = minhaBio;
-document.getElementById("anoFormatura").innerText = "Ano de Formatura: " + anoFormatura
+document.getElementById("anoFormatura").innerText = "Ano de formatura: " + anoFormatura;
+document.getElementById("tempoRestanteParaFormatura").innerText = Tempo restante para formatura: ${anoFormatura - anoAtual} anos;
